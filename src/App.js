904baf99df9a995ae9a75b5b069c3ad03b69872f
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2025-10-25 14:01:42 +0200

    Added a search engine

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="app-header">
   <img
     alt="SheCodes"
     className="logo"
     src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
   />
-</header>
-      <h1 className='text-center'>Dictionary</h1>
+  </header>
+  <div>
+   <h1 className='text-center'>Dictionary</h1>
+   </div>
+   
+   <main>
+    <div>
+    <Dictionary />
+    </div>
+   </main>
+
+   <footer className='text-center'>Coded by <a href="https://react-dictionary-ap.netlify.app/">Moisha💕</a></footer>
+      
+      </div>
     </div>
   );
 }
