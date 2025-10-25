19c4c8f407a3b33c6661bde1b17fde8a9042c6e8
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2025-10-25 13:14:37 +0200

    Added a logo and a header

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+  <img
+    alt="SheCodes"
+    className="logo"
+    src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
+  />
+</header>
+      <h1 className='text-center'>Dictionary</h1>
+    </div>
+  );
+}
+
+
